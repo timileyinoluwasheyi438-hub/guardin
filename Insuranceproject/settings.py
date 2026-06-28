446e3244f3964a8bd919fc4cd2727c84041c59c8
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-06-28 13:01:08 +0100

    fix settings

--- Insuranceproject/settings.py
+++ Insuranceproject/settings.py
@@ -19,8 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     'insurance',
     
